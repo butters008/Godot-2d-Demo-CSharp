c0e9ac0efe52156937a27c201e4eb070bba28c5e
Butters <k.butterfield08@gmail.com>
2022-07-12 07:58:56 -0500

    last night work

--- 2D Platformer/CharacterAndCam Move/Scripts/BobV2.cs
+++ 2D Platformer/CharacterAndCam Move/Scripts/BobV2.cs
@@ -6,20 +6,30 @@ public class BobV2 : KinematicBody2D
   //Declare vars here
   const float GRAVITY = 200f;
   const int SPEED = 100;
-
   Vector2 velocity;
+	AnimationPlayer animationPlayer;
 
   public override void _Ready(){
-	GD.Print("Getting the Ready Ready...");
+		GD.Print("Getting the Ready Ready...");
+		animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		//Testing
   }
 
   public void getInput(){
-	if(Input.IsActionJustPressed("move_right")){
-	  velocity.x = SPEED;
-	}
-	if(Input.IsActionJustPressed("move_left")){
-	  velocity.x = -SPEED;
-	}
+		if(Input.IsActionJustPressed("move_right")){
+			velocity.x = SPEED;
+			animationPlayer.Play("Walk");
+			GetNode<Sprite>("Sprite").FlipH = false;
+		}
+		else if(Input.IsActionJustPressed("move_left")){
+			velocity.x = -SPEED;
+			animationPlayer.Play("Walk");
+			GetNode<Sprite>("Sprite").FlipH = true;
+		}
+		else{
+			animationPlayer.Play("Idle");
+			velocity.x = Mathf.Lerp(velocity.x, 0, 0.25f);
+		}
   }
 
   public override void _PhysicsProcess(float delta)
